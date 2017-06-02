0e0439e429a8c4a24d5011fe4f47282406d4b02d
nativanando <nativanando@gmail.com>
2017-06-02 17:37:41 -0300

    modificando codigo concorrente

--- Server/ServerConnection.py
+++ Server/ServerConnection.py
@@ -1,33 +1,25 @@
 import socket
-import _thread
-HOST = '127.0.0.1'              # Endereco IP do Servidor
+import os
+import sys
+HOST = '127.0.0.1'             # Endereco IP do Servidor
 PORT = 5555            # Porta que o Servidor esta
-
-def connected(con, client):
-    print ('Conectado por', client)
-    while True:
-        message = con.recv(1024)
-        if not message: break
-        print (client, message.decode())
-
-    print ('Finalizando conexao do cliente', client)
-    con.close()
-    _thread.exit()
-
-def openConnection():
-    tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server = (HOST, PORT)
-    tcp.bind(server)
-    tcp.listen(1)
-    print("servidor aberto em", server)
-    createThread(tcp)
-
-def createThread(tcp):
-    while True:
-        con, client = tcp.accept()
-        _thread.start_new_thread(connected(con, client))
-    tcp.close()
-
-def startServer():
-    openConnection()
-
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+orig = (HOST, PORT)
+tcp.bind(orig)
+tcp.listen(1)
+while True:
+    con, cliente = tcp.accept()
+    pid = os.fork()
+    print (pid)
+    if pid == 0:
+        tcp.close()
+        print ('Conectado por', cliente)
+        while True:
+            msg = con.recv(1024)
+            if not msg: break
+            print (cliente, msg)
+        print ('Finalizando conexao do cliente', cliente)
+        con.close()
+        sys.exit(0)
+    else:
+        con.close()
